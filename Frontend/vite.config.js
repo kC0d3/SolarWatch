869d483036832bf1f92afe4cee6255243486ec74
kC0d3 <kC0d3@github.com>
2024-02-28 21:58:36 +0100

    Change target proxy.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5240',
+        target: 'https://solarwatch-solar-backend.onrender.com/',
         changeOrigin: true,
       },
     },
